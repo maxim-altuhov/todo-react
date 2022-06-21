551fa43c17da1d970fb8d87ad0001d804c7cc676
Maxim A <m_altuhov@mail.ru>
2022-06-21 10:47:32 +0300

    Создал основу интерфейса

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import listIcon from './assets/img/list.svg';
+
 function App() {
   return (
     <div className="todo">
       <div className="todo__sidebar">
-
+        <ul className="todo__list">
+          <li className="todo__list-item todo__list-item_active">
+            <i className="todo__list-icon">
+              <img src={listIcon} alt="List icon" />
+            </i>
+            <span className="todo__list-label">Все задачи</span>
+          </li>
+        </ul>
       </div>
-      <div className="todo__content">
-        
-      </div>
+      <div className="todo__tasks">456</div>
     </div>
   );
 }
